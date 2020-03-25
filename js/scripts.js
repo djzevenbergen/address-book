450f5054e26f5fae1cb48dfcab6225861f915f90
djzevenbergen <djzevenbergen@gmail.com>
2020-03-25 15:18:05 -0700

    fix issues and get working

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,16 @@ $(document).ready(function() {
     var phoneNumber = $("input#phoneNumber").val();
     var email = $("input#email").val();
     event.preventDefault();
-    $("#first").after("<p class=\"blue\">" + name + "</p>"); // + "<ul>" + "<li>" + address + "</li>" + "<li>" + phoneNumber + "</li>" + "<li>" + Email + "</li>" + "</ul>" );
-    $(".blue").after("<ul> <li> " + address + " </li>  <li> " + phoneNumber + " <li> " + email + " </li>  </ul>");
+    $("#first").after("<div id=\"con\"> <p class=\"blue\">" + name + "</p>" + "<ul class=\"hidden\"> <li> " + address + " </li>  <li> " + phoneNumber + " <li> " + email + " </li>  </ul> </div>"); // + "<ul>" + "<li>" + address + "</li>" + "<li>" + phoneNumber + "</li>" + "<li>" + Email + "</li>" + "</ul>" );
+    //$(".blue").after("<ul> <li> " + address + " </li>  <li> " + phoneNumber + " <li> " + email + " </li>  </ul>");
     //$(".contacts").append()
 
     event.preventDefault();
 
-    $(".contactIndex").click(function() {
-      $(this).children(ul).toggleClass("hidden");
+    $("#con").click(function() {
+      $(this).children("ul").toggleClass("hidden");
     });
-  
 
   });
-
-
 
 });
